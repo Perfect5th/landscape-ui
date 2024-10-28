6497e5bb3b794c6cf9437a907c1edbb51bcca567
Mitch Burton <mitch.burton@canonical.com>
2024-10-28 14:02:04 +0000

    automated build

--- assets/NoData-CyXo1XC7.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BIsTIai5.js";const o=()=>r.jsx(r.Fragment,{children:"---"});export{o as N};
---
+++ assets/useFetchOld-BnInaXWV.js
@@ -0,0 +1,1 @@
+import{r as t,F as e}from"./index-BboLnx8e.js";function o(){return t.useContext(e)}export{o as u};
