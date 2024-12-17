6bbfb337df62af62d5fe0f0c333244cbe684d582
Mitch Burton <mitch.burton@canonical.com>
2024-12-17 17:28:07 +0000

    automated build

---
+++ assets/GroupFilter-CN3d3M-a.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-DnCwBEud.js";import{u as a}from"./usePageParams-DYqnV1GS.js";import{T as m}from"./TableFilterChips-PORlBZdm.js";const g=({options:e})=>{const{groupBy:r,setPageParams:t}=a();return s.jsx(m,{multiple:!1,label:"Group by",hasToggleIcon:!0,hasBadge:!0,options:e,onItemSelect:o=>t({groupBy:o}),selectedItem:r})};export{g as G};
