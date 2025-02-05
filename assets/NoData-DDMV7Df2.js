a044b564032be07c35440ae356ca99c2807d31df
Mitch Burton <mitch.burton@canonical.com>
2025-02-05 15:11:14 +0000

    automated build

---
+++ assets/NoData-DDMV7Df2.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BPx7bxy4.js";const r="---",s=()=>t.jsx(t.Fragment,{children:r});export{s as N};
--- assets/SidePanelTablePagination-Dq0y_3-C.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-DCAwryRC.js";import{T as m}from"./TablePaginationBase-BsWWrGU5.js";const j=({currentPage:a,pageSize:o,paginate:t,setPageSize:e,totalItems:i,className:n="",currentItemCount:r=0})=>s.jsx(m,{className:n,currentItemCount:r,currentPage:a,pageSize:o,paginate:t,setPageSize:e,totalItems:i});export{j as S};
