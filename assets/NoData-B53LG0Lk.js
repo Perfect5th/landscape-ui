1c9124e670dca47d5b13c21f6fe4f353712aa7c0
Mitch Burton <mitch.burton@canonical.com>
2025-02-25 15:12:43 +0000

    automated build

--- assets/NoData-B53LG0Lk.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-D7L52Usx.js";const r="---",s=()=>t.jsx(t.Fragment,{children:r});export{s as N};
---
+++ assets/index-CNvr8E43.js
@@ -0,0 +1,1 @@
+import{a,r}from"./index-CeW626Nl.js";const s=()=>{const e=a();return r.useEffect(()=>{e("/profiles/package",{replace:!0})},[]),null};export{s as default};
