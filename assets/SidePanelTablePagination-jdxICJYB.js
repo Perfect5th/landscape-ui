cfb42c28bd2c194f3451fda5de6bef1d84a4258e
Mitch Burton <mitch.burton@canonical.com>
2025-03-03 11:35:46 +0000

    automated build

--- assets/SidePanelTablePagination-jdxICJYB.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-CeW626Nl.js";import{T as m}from"./TablePaginationBase-DgRvO5qb.js";const j=({currentPage:a,pageSize:o,paginate:t,setPageSize:e,totalItems:i,className:n="",currentItemCount:r=0})=>s.jsx(m,{className:n,currentItemCount:r,currentPage:a,pageSize:o,paginate:t,setPageSize:e,totalItems:i});export{j as S};
--- assets/index-CNvr8E43.js
+++
@@ -1,1 +0,0 @@
-import{a,r}from"./index-CeW626Nl.js";const s=()=>{const e=a();return r.useEffect(()=>{e("/profiles/package",{replace:!0})},[]),null};export{s as default};
