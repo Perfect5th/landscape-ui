f30f5a9a9db004574acffe42b089f29b1031d568
Mitch Burton <mitch.burton@canonical.com>
2025-05-02 15:18:39 +0000

    automated build

--- assets/index-B42Fjhnd.js
+++
@@ -1,1 +0,0 @@
-import{a,r}from"./index-Co97hBNM.js";const s=()=>{const e=a();return r.useEffect(()=>{e("/profiles/package",{replace:!0})},[]),null};export{s as default};
---
+++ assets/index-DF8hR9wb.js
@@ -0,0 +1,1 @@
+import{j as t,m as r}from"./index-AQFe3UL3.js";const s=({code:e})=>t.jsx(r.CodeSnippet,{blocks:[{title:"Code preview",code:e,wrapLines:!0,appearance:"numbered"}]});export{s as default};
