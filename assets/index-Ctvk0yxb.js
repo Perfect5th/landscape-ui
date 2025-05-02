b0fe7a69139f7cb212abc6fa2d7e1ae9f92482e0
Mitch Burton <mitch.burton@canonical.com>
2025-05-02 19:27:37 +0000

    automated build

--- assets/index-Ctvk0yxb.js
+++
@@ -1,1 +0,0 @@
-import{a as r,r as t}from"./index-Df03TKU2.js";const s=()=>{const e=r();return t.useEffect(()=>{e("/repositories/mirrors",{replace:!0})},[]),null};export{s as default};
--- assets/index-DAHOwMCl.js
+++
@@ -1,1 +0,0 @@
-import{a,r as t}from"./index-Df03TKU2.js";const c=()=>{const e=a();return t.useEffect(()=>{e("/account/general",{replace:!0})},[]),null};export{c as default};
