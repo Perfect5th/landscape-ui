c86b748c6a36b4d3bbbf18398c36fe8a3920502b
Mitch Burton <mitch.burton@canonical.com>
2025-05-16 08:55:58 +0000

    automated build

--- assets/index-5NLRvkaY.js
+++
@@ -1,1 +0,0 @@
-import{r as a,j as c,B as d}from"./index-CL5bVZIc.js";const o=({instanceId:t})=>{const[e,s]=a.useState([]);return c.jsx(d,{instanceId:t,selectedIds:e,setSelectedIds:r=>s(r)})};export{o as default};
