2054a5a624d502ca96746196445a8d7a68abb4f7
Mitch Burton <mitch.burton@canonical.com>
2025-05-23 16:02:20 +0000

    automated build

--- assets/index-CahYO-uz.js
+++
@@ -1,1 +0,0 @@
-import{a,r as t}from"./index-BnS9w30E.js";const c=()=>{const e=a();return t.useEffect(()=>{e("/account/general",{replace:!0})},[]),null};export{c as default};
--- assets/index-D_S-PbSU.js
+++
@@ -1,1 +0,0 @@
-import{a,r}from"./index-BnS9w30E.js";const s=()=>{const e=a();return r.useEffect(()=>{e("/profiles/package",{replace:!0})},[]),null};export{s as default};
