2d3ba6ea0d02023d76e02a339d585322a7d66278
Mitch Burton <mitch.burton@canonical.com>
2025-06-12 17:06:16 +0000

    automated build

--- assets/PageNotFound-Bhp6cy0W.js
+++
@@ -1,1 +0,0 @@
-import{j as t,L as o}from"./index-C9oe_qE4.js";import{A as e}from"./AuthTemplate-NIy35b9W.js";const n=()=>t.jsxs(e,{title:"Page not found",children:[t.jsx("p",{children:"It seems that page you're looking for doesn't exist."}),t.jsx(o,{to:"/",className:"p-button--positive u-no-margin--bottom",children:"Go back to the home page"})]});export{n as default};
