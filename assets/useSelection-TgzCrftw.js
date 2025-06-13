cf44aa4bdd41be5b7f2d7b2498d2e4c30ef77bc7
Mitch Burton <mitch.burton@canonical.com>
2025-06-13 15:29:22 +0000

    automated build

--- assets/useSelection-TgzCrftw.js
+++
@@ -1,1 +0,0 @@
-import{k as u,r as t}from"./index-B8POXmbS.js";const l=(e,o)=>{const{closeSidePanel:r}=u(),[c,a]=t.useState([]),[n,s]=t.useState([]);return e!=c&&(r(),o||(s([]),a(e))),{selectedItems:n,setSelectedItems:s}};export{l as u};
