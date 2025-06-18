05a1f500c377f4f86651be81d58069e28367eb77
Mitch Burton <mitch.burton@canonical.com>
2025-06-18 20:48:49 +0000

    automated build

--- assets/index-CQDmBXJs.js
+++
@@ -1,1 +0,0 @@
-import{j as t,n as r}from"./index-C2g5yNkj.js";const s=({code:e})=>t.jsx(r.CodeSnippet,{blocks:[{title:"Code preview",code:e,wrapLines:!0,appearance:"numbered"}]});export{s as default};
