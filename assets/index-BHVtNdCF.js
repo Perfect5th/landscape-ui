1f04f1364edf7e69e68c42d51fca2a5b78de5dea
Mitch Burton <mitch.burton@canonical.com>
2025-07-31 22:19:43 +0000

    automated build

--- assets/index-BHVtNdCF.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BSTk3ARV.js";import{A as t}from"./AuthTemplate-Bpw7oHTr.js";import{A as i}from"./AvailableProviderList-Cq2IbEHF.js";const s=()=>e.jsx(t,{title:"Sign in to Landscape",children:e.jsx(i,{isStandaloneOidcEnabled:!1,isUbuntuOneEnabled:!0,oidcProviders:[]})});export{s as default};
--- assets/useSelection-Dl5-zlMF.js
+++
@@ -1,1 +0,0 @@
-import{i as s}from"./index-BSTk3ARV.js";const n=(e,o)=>{const[r,c]=s.useState(e),[u,t]=s.useState([]);return e!==r&&!o&&(t([]),c(e)),{selectedItems:u,setSelectedItems:t}};export{n as u};
