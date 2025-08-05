c26c3cd53c4fcd7f99c440493b07385a0fabc89e
Mitch Burton <mitch.burton@canonical.com>
2025-08-05 14:58:05 +0000

    automated build

---
+++ assets/index-C5_52SW0.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DUhs-kzo.js";import{A as t}from"./AuthTemplate-CNZunoxr.js";import{A as i}from"./AvailableProviderList-DmkTlU5x.js";const s=()=>e.jsx(t,{title:"Sign in to Landscape",children:e.jsx(i,{isStandaloneOidcEnabled:!1,isUbuntuOneEnabled:!0,oidcProviders:[]})});export{s as default};
