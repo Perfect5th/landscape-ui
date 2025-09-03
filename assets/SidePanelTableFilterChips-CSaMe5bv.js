a641d0f2f08589d7c4e62a420d9a03c7143c1c08
Mitch Burton <mitch.burton@canonical.com>
2025-09-03 16:22:49 +0000

    automated build

---
+++ assets/SidePanelTableFilterChips-CSaMe5bv.js
@@ -0,0 +1,1 @@
+import{j as s,G as r}from"./index-D5Rc4jDZ.js";const a=e=>s.jsx(r,{...e});export{a as S};
---
+++ assets/useGetTags-ZFrMb8U5.js
@@ -0,0 +1,1 @@
+import{b as a,i as n}from"./index-D5Rc4jDZ.js";const r=()=>{const t=a(),{data:s,isPending:e}=n({queryKey:["instanceTags"],queryFn:async()=>t.get("tags")});return{tags:s?.data.results??[],tagsCount:s?.data.count,isGettingTags:e}};export{r as u};
