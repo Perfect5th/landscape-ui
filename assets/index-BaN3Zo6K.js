755372f81b43a18456656c20df07b2ba2a9f76d2
ethanashaw <189650945+ethanashaw@users.noreply.github.com>
2025-10-24 16:48:14 +0000

    Automated build: b43af61d11662bc4c9055f2aee6b6dbb4893d151

---
+++ assets/index-BaN3Zo6K.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-b6eKq6Xg.js";import{A as t}from"./AuthTemplate-DDwGcJQW.js";import{A as i}from"./AvailableProviderList-Dz_lTPm_.js";const s=()=>e.jsx(t,{title:"Sign in to Landscape",children:e.jsx(i,{isStandaloneOidcEnabled:!1,isUbuntuOneEnabled:!0,oidcProviders:[]})});export{s as default};
---
+++ assets/useGetInstance-B49nFrkz.js
@@ -0,0 +1,1 @@
+import{m as u,f as o}from"./index-b6eKq6Xg.js";const i=({instanceId:e,...t},s)=>{const n=u(),{data:a,isLoading:c}=o({queryKey:["instances",e,t],queryFn:async()=>n.get(`computers/${e}`,{params:t}),...s});return{instance:a?.data,isGettingInstance:c}};export{i as u};
